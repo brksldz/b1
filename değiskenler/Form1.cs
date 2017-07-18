95f2de6753c1c95abc2ce9c312de645600985670
burak.salduz <brksldz@hotmail.com>
2017-07-18 20:43:56 +0300

    değişkenler dersi

--- değiskenler/Form1.cs
+++ değiskenler/Form1.cs
@@ -12,9 +12,32 @@ namespace değiskenler
 {
     public partial class Form1 : Form
     {
+        //değişkenler,verileri kaydettiğimiz alanlardır
+
+        //sayısal veri tipleri
+
+        byte sayi; //bu sayıya 0-255 arası bir değer ataması yapılabilir.
+        string metin;
         public Form1()
         {
             InitializeComponent();
+
+
+            sayi = 5;
+            metin = "2017 yılındayız.";
+       
+            
+        }
+        
+        private void button1_Click(object sender,EventArgs e)
+        {
+            MessageBox.Show(sayi.ToString());
+            sayi = 10;
+            MessageBox.Show(sayi.ToString());
+            MessageBox.Show(metin);
+        
+
+
         }
     }
 }
